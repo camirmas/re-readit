ba920cfca3e5ac3d97130b00ab42e7752703fad3
Cameron Irmas <cjirmas@gmail.com>
2015-03-16 09:15:38 -0700

    refactor router; style

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,5 +1,8 @@
 Template.postItem.helpers({
   postURL: function() {
     return '/r/' + this.data.subreddit + '/post/' + this.data.id;
+  },
+  thumbnail: function() {
+    return this.data.thumbnail || "http://placehold.it/140x100&text=hello+:)";
   }
 });
