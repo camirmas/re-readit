97b4dd3903f2973bc2a4b7a202c3d17ee03afea7
Cameron Irmas <cjirmas@gmail.com>
2015-03-21 18:44:12 -0700

    users can edit boards

--- client/templates/boards/board_card.js
+++ client/templates/boards/board_card.js
@@ -17,5 +17,18 @@ Template.boardCard.events({
       });
       Meteor.call('boardDelete', _this);
     }
+  },
+  'click .board-edit': function(e) {
+    $(e.target).parent().next('.board-edit-form').find('.name-input').val(this.name);
+    $(e.target).parent().next('.board-edit-form').removeClass('hidden');
+  },
+  'click .cancel-update': function(e) {
+    $('.board-edit-form').addClass('hidden');
+  },
+  'click .complete-update': function(e) {
+    var input = $(e.target).prev().find('.name-input').val();
+    Meteor.call('boardUpdate', this, input);
+    $('.name-input').val("");
+    $('.board-edit-form').addClass('hidden');
   }
 });
