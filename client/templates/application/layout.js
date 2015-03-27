e4106bb3fead68f4054f3c9686273c722d9bf3f4
msmykowski <smycal@gmail.com>
2015-03-27 10:58:37 -0700

    add following notification and badge icons

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -1,5 +1,19 @@
 Template.layout.helpers({
   currentUrl: function() {
     return 'users/' + Meteor.user()._id;
-  }
+  },
+  unreadNotifications: function() {
+    var user = Meteor.user()._id;
+    return Notifications.find({followers: user, read: false}).count();
+  },
 });
+
+Template.layout.events({
+  'click .unreadNotif': function() {
+    var user = Meteor.user()._id;
+    var notifications = Notifications.find({followers: user, read: false});
+    notifications.forEach(function(notification) {
+      Notifications.update(notification._id, {$set: {read: true}})
+    });
+  }
+})
